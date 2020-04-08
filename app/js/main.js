e243767ae05865d456731879575f450f0ec5a92e
Tatyana Shurinova <tatyana.shurinova@gmail.com>
2020-04-08 12:07:19 +0300

    finished section slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function () {
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true,
+  });
+
+  $(".product-slider-inner").slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
+
   var mixer = mixitup(".products__inner-box");
 });
