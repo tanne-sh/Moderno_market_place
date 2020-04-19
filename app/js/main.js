d7f28332bdda4d2ef625d760c3a0c02d5cec3004
Tatyana Shurinova <tatyana.shurinova@gmail.com>
2020-04-19 12:18:59 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -33,5 +33,13 @@ $(function () {
     $(".icon-th-list").removeClass("active");
   });
 
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").slideToggle();
+  });
+
+  $(".header_btn-menu").on("click", function () {
+    $(".header__box").toggleClass("active");
+  });
+
   var mixer = mixitup(".products__inner-box");
 });
